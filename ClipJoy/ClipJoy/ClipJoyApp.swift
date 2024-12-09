ec9cf261318143f21a7ec0d6c915683617a92524
Sabih <sabih.ulhasan@yahoo.com>
2024-12-09 12:05:52 +0100

    Created XCode Project

---
+++ ClipJoy/ClipJoy/ClipJoyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipJoyApp.swift
+//  ClipJoy
+//
+//  Created by Muhammad Sabihul Hasan on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ClipJoyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipJoy/ClipJoy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClipJoy
+//
+//  Created by Muhammad Sabihul Hasan on 09/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
